707390bfed2684e12f2194a4aa14c71810195709
rafkudyba <160314@stud.prz.edu.pl>
2021-06-19 12:39:27 +0200

    handlers

---
+++ PoznajRzeszow.Application/Queries/Visits/GetVisitedPlaces/GetVisitedPlacesQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoznajRzeszow.Application.Queries.Visits.GetVisitedPlaces
+{
+    public class GetVisitedPlacesQuery : IRequest<List<VisitedPlacesDto>>
+    {
+        public Guid UserId { get; set; }
+        
+    }
+}
---
+++ PoznajRzeszow.Application/Queries/Visits/GetVisitedPlaces/VisitedPlacesDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoznajRzeszow.Application.Queries.Visits.GetVisitedPlaces
+{
+
+    public class VisitedPlacesDto
+    {
+        public VisitedPlacesDto(Guid placeId, string name, string address,string directoryPath)
+        {
+            PlaceId = placeId;
+            Name = name;
+            Address = address;
+            DirectoryPath = directoryPath;
+        }
+
+        public Guid PlaceId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string DirectoryPath { get; set; }
+    }
+}
